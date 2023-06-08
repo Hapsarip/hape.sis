52f34af943622d2ca71e15eff0a008dfcde8ce97
Hapsarip <hapsari.ndhari7@gmail.com>
2023-06-08 17:35:29 +0700

    fixing logic in rekomendasi.js

--- index.php
+++ index.php
@@ -86,13 +86,6 @@
 	
     <!-- Body End -->
 
-    <!-- Footer Start -->
-	<div class="footer-copyright" style="padding: 0px 0px">
-      <div class="container">
-        <p align="center" style="color: #999">&copy; Sistem Pendukung Keputusan Pemilihan Smartphone 2018.</p>
-      </div>
-    </div>
-    <!-- Footer End -->
     <script type="text/javascript">
 	 			$(document).ready(function(){
 	      $('.parallax').parallax();
