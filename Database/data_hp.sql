60b4264815033d26323ff2ed56806f0cf7e8d470
Hapsarip <hapsari.ndhari7@gmail.com>
2023-06-11 05:10:14 +0700

    fixing margin, hasil page, and daftar_hp page

--- Database/data_hp.sql
+++ Database/data_hp.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 23, 2018 at 01:57 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hp`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `data_hp`
---
 
 CREATE TABLE `data_hp` (
   `id_hp` int(4) NOT NULL,
@@ -42,10 +17,6 @@ CREATE TABLE `data_hp` (
   `processor_angka` varchar(64) COLLATE utf8_bin NOT NULL,
   `kamera_angka` varchar(64) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `data_hp`
---
 
 INSERT INTO `data_hp` (`id_hp`, `nama_hp`, `harga_hp`, `ram_hp`, `memori_hp`, `processor_hp`, `kamera_hp`, `harga_angka`, `ram_angka`, `memori_angka`, `processor_angka`, `kamera_angka`) VALUES
 (1, 'Samsung Galaxy S23', '12999000', '8', '128', 'Octacore', '50', '2', '3',	'3', '3',	'4'),
@@ -64,26 +35,8 @@ INSERT INTO `data_hp` (`id_hp`, `nama_hp`, `harga_hp`, `ram_hp`, `memori_hp`, `p
 (14, 'Samsung Galaxy S21 Ultra', '20000000', '12', '512', 'Octacore', '108', '1',	'4',	'5',	'3',	'5'),
 (15, 'OnePlus 8T', '8999000', '12', '256', 'Octacore', '48', '3',	'4',	'4',	'3',	'4');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `data_hp`
---
 ALTER TABLE `data_hp`
   ADD PRIMARY KEY (`id_hp`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `data_hp`
---
 ALTER TABLE `data_hp`
   MODIFY `id_hp` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
